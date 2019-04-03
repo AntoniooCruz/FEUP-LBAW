cecff746cea38e814574c4e67abe25e0222f7732
Beatriz Soares Mendes <up201604253@fe.up.pt>
2019-04-03 10:51:08 +0100

    Update lbaw_db.sql

--- database/lbaw_db.sql
+++ database/lbaw_db.sql
@@ -5,44 +5,31 @@ CREATE TYPE verification_state AS ENUM ('Pending', 'Approved');
 DROP TYPE IF EXISTS report_state;
 CREATE TYPE report_state AS ENUM ('Pending', 'Approved', 'Ignored');
 
+DROP TYPE IF EXISTS report_types;
+CREATE TYPE report_types AS ENUM ('Post', 'Event', 'User');
+
 
 -- Table: users
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id_user  INTEGER      PRIMARY KEY,
+    id_user  SERIAL      PRIMARY KEY,
     username VARCHAR (15) UNIQUE
                           NOT NULL,
     email    VARCHAR      UNIQUE
                           NOT NULL,
     name     VARCHAR (20) NOT NULL,
-    password VARCHAR      NOT NULL
+    password VARCHAR      NOT NULL,
+    description VARCHAR (100),
+    active BOOLEAN default(true),
+    is_admin BOOLEAN NOT NULL
 );
-
-
--- Table: member
-DROP TABLE IF EXISTS member;
-
-CREATE TABLE member (
-    id_user     INTEGER       PRIMARY KEY
-                              REFERENCES users (id_user) ON DELETE CASCADE,
-    description VARCHAR (100) 
-);
-
--- Table: admin
-DROP TABLE IF EXISTS admin;
-
-CREATE TABLE admin (
-    id_user INTEGER PRIMARY KEY
-                  REFERENCES users (id_user) ON DELETE CASCADE
-);
-
 
 -- Table: category
 DROP TABLE IF EXISTS category;
 
 CREATE TABLE category (
-    id_category INTEGER      PRIMARY KEY,
+    id_category SERIAL      PRIMARY KEY,
     name        VARCHAR (20) UNIQUE
                              NOT NULL
 );
@@ -52,7 +39,7 @@ CREATE TABLE category (
 DROP TABLE IF EXISTS event;
 
 CREATE TABLE event (
-    id_event     INTEGER       PRIMARY KEY,
+    id_event     SERIAL       PRIMARY KEY,
     title        VARCHAR (50)  NOT NULL,
     date_created DATE          NOT NULL
                                DEFAULT (now() ),
@@ -63,7 +50,7 @@ CREATE TABLE event (
                                CONSTRAINT price_value CHECK (price >= 0),
     capacity     INTEGER       CONSTRAINT capacity_value CHECK (capacity > 0),
     isPrivate    BOOLEAN       NOT NULL,
-    id_owner     INTEGER       REFERENCES member (id_user) ON DELETE CASCADE,
+    id_owner     INTEGER       REFERENCES users (id_user) ON DELETE CASCADE,
     id_category  INTEGER       REFERENCES category (id_category) ON DELETE CASCADE,
     city         VARCHAR (30),
 
@@ -75,13 +62,13 @@ CREATE TABLE event (
 DROP TABLE IF EXISTS post;
 
 CREATE TABLE post (
-    id_post   INTEGER       PRIMARY KEY,
+    id_post   SERIAL       PRIMARY KEY,
     date      DATE          NOT NULL
                             DEFAULT (now() ),
     text      VARCHAR (140),
     id_event  INTEGER       REFERENCES event (id_event) ON DELETE CASCADE
                             NOT NULL,
-    id_author INTEGER       REFERENCES member (id_user) ON DELETE CASCADE
+    id_author INTEGER       REFERENCES users (id_user) ON DELETE CASCADE
                             NOT NULL
 );
 
@@ -90,10 +77,10 @@ CREATE TABLE post (
 DROP TABLE IF EXISTS attend_event;
 
 CREATE TABLE attend_event (
-    id_member INTEGER REFERENCES member (id_user) ON DELETE CASCADE,
+    id_user INTEGER REFERENCES users (id_user) ON DELETE CASCADE,
     id_event  INTEGER REFERENCES event (id_event) ON DELETE CASCADE,
     PRIMARY KEY (
-        id_member,
+        id_user,
         id_event
     )
 );
@@ -103,8 +90,8 @@ CREATE TABLE attend_event (
 DROP TABLE IF EXISTS business;
 
 CREATE TABLE business (
-    id_member    INTEGER PRIMARY KEY
-                         REFERENCES member (id_user) ON DELETE CASCADE,
+    id_user    INTEGER PRIMARY KEY
+                         REFERENCES users (id_user) ON DELETE CASCADE,
     verification verification_state    DEFAULT ('Pending') 
                          NOT NULL,
     website      TEXT
@@ -114,26 +101,8 @@ CREATE TABLE business (
 DROP TABLE IF EXISTS personal;
 
 CREATE TABLE personal (
-    id_member INTEGER PRIMARY KEY
-                    REFERENCES Member (id_user) ON DELETE CASCADE
-);
-
-
--- Table: banned_business
-DROP TABLE IF EXISTS banned_business;
-
-CREATE TABLE banned_business (
-    id_business INTEGER REFERENCES business (id_member) ON DELETE CASCADE
-                      PRIMARY KEY
-);
-
-
--- Table: banned_personal
-DROP TABLE IF EXISTS banned_personal;
-
-CREATE TABLE banned_personal (
-    id_personal INTEGER PRIMARY KEY
-                      REFERENCES personal (id_member) ON DELETE CASCADE
+    id_user INTEGER PRIMARY KEY
+                    REFERENCES users (id_user) ON DELETE CASCADE
 );
 
 
@@ -141,7 +110,7 @@ CREATE TABLE banned_personal (
 DROP TABLE IF EXISTS comment;
 
 CREATE TABLE comment (
-    id_comment        INTEGER      PRIMARY KEY,
+    id_comment        SERIAL      PRIMARY KEY,
     text              VARCHAR (50) NOT NULL,
     id_post           INTEGER      NOT NULL
                                    REFERENCES post (id_post) ON DELETE CASCADE,
@@ -156,7 +125,7 @@ CREATE TABLE comment (
 DROP TABLE IF EXISTS file;
 
 CREATE TABLE file (
-    id_file INTEGER PRIMARY KEY,
+    id_file SERIAL PRIMARY KEY,
     name    VARCHAR NOT NULL,
     id_post INTEGER NOT NULL
                     REFERENCES post (id_post) ON DELETE CASCADE
@@ -167,8 +136,8 @@ CREATE TABLE file (
 DROP TABLE IF EXISTS follow;
 
 CREATE TABLE follow (
-    id_user1 INTEGER REFERENCES personal (id_member) ON DELETE CASCADE,
-    id_user2 INTEGER REFERENCES member (id_user) ON DELETE CASCADE,
+    id_user1 INTEGER REFERENCES personal (id_user) ON DELETE CASCADE,
+    id_user2 INTEGER REFERENCES users (id_user) ON DELETE CASCADE,
     PRIMARY KEY (
         id_user1,
         id_user2
@@ -180,8 +149,8 @@ CREATE TABLE follow (
 DROP TABLE IF EXISTS invite;
 
 CREATE TABLE invite (
-    id_inviter INTEGER REFERENCES personal (id_member) ON DELETE CASCADE,
-    id_invitee INTEGER REFERENCES personal (id_member) ON DELETE CASCADE,
+    id_inviter INTEGER REFERENCES personal (id_user) ON DELETE CASCADE,
+    id_invitee INTEGER REFERENCES personal (id_user) ON DELETE CASCADE,
     id_event   INTEGER REFERENCES event (id_event) ON DELETE CASCADE,
     PRIMARY KEY (
         id_inviter,
@@ -195,23 +164,13 @@ CREATE TABLE invite (
 DROP TABLE IF EXISTS report;
 
 CREATE TABLE report (
-    id_report INTEGER      PRIMARY KEY,
+    id_report SERIAL      PRIMARY KEY,
     reason    VARCHAR (50) NOT NULL,
     veridict  report_state DEFAULT ('Pending') 
-                           NOT NULL
-);
-
-
--- Table: judge_report
-DROP TABLE IF EXISTS judge_report;
-
-CREATE TABLE judge_report (
-    id_report INTEGER REFERENCES report (id_report) ON DELETE CASCADE,
-    id_admin  INTEGER REFERENCES admin (id_user) ON DELETE CASCADE,
-    PRIMARY KEY (
-        id_report,
-        id_admin
-    )
+                           NOT NULL,
+    report_type report_types,
+    id_admin INTEGER REFERENCES users (id_user) ON DELETE CASCADE
+                    NOT NULL
 );
 
 
@@ -219,7 +178,7 @@ CREATE TABLE judge_report (
 DROP TABLE IF EXISTS poll;
 
 CREATE TABLE poll (
-    id_poll INTEGER PRIMARY KEY,
+    id_poll SERIAL PRIMARY KEY,
     id_post INTEGER REFERENCES post (id_post) ON DELETE CASCADE
                     NOT NULL
 );
@@ -229,7 +188,7 @@ CREATE TABLE poll (
 DROP TABLE IF EXISTS poll_option;
 
 CREATE TABLE poll_option (
-    id_poll_option INTEGER      PRIMARY KEY,
+    id_poll_option SERIAL      PRIMARY KEY,
     name           VARCHAR (20) NOT NULL,
     id_poll        INTEGER      REFERENCES poll (id_poll) ON DELETE CASCADE
                                 NOT NULL
@@ -243,7 +202,7 @@ CREATE TABLE report_event (
     id_report   INTEGER PRIMARY KEY
                         REFERENCES report (id_report) ON DELETE CASCADE,
     id_reporter INTEGER NOT NULL
-                        REFERENCES member (id_user) ON DELETE CASCADE,
+                        REFERENCES users (id_user) ON DELETE CASCADE,
     id_event    INTEGER NOT NULL
                         REFERENCES event (id_event) ON DELETE CASCADE
 );
@@ -256,7 +215,7 @@ CREATE TABLE report_post (
     id_report   INTEGER PRIMARY KEY
                         REFERENCES report (id_report) ON DELETE CASCADE,
     id_reporter INTEGER NOT NULL
-                        REFERENCES member (id_user) ON DELETE CASCADE,
+                        REFERENCES users (id_user) ON DELETE CASCADE,
     id_post     INTEGER REFERENCES post (id_post) ON DELETE CASCADE
                         NOT NULL
 );
@@ -268,9 +227,9 @@ DROP TABLE IF EXISTS report_user;
 CREATE TABLE report_user (
     id_report          INTEGER REFERENCES report (id_report) ON DELETE CASCADE
                                PRIMARY KEY,
-    id_reporter        INTEGER REFERENCES member (id_user) ON DELETE CASCADE
+    id_reporter        INTEGER REFERENCES users (id_user) ON DELETE CASCADE
                                NOT NULL,
-    id_reported_member INTEGER REFERENCES member (id_user) ON DELETE CASCADE
+    id_reported_user INTEGER REFERENCES users (id_user) ON DELETE CASCADE
                                NOT NULL
 );
 
@@ -279,10 +238,10 @@ CREATE TABLE report_user (
 DROP TABLE IF EXISTS ticket;
 
 CREATE TABLE ticket (
-    token           INTEGER PRIMARY KEY,
+    token           SERIAL PRIMARY KEY,
     id_event        INTEGER REFERENCES event (id_event) ON DELETE CASCADE
                             NOT NULL,
-    id_ticket_owner INTEGER REFERENCES personal (id_member) ON DELETE CASCADE
+    id_ticket_owner INTEGER REFERENCES personal (id_user) ON DELETE CASCADE
                             NOT NULL
 );
 
@@ -291,7 +250,7 @@ CREATE TABLE ticket (
 DROP TABLE IF EXISTS vote_on_poll;
 
 CREATE TABLE vote_on_poll (
-    id_user        INTEGER REFERENCES member (id_user) ON DELETE CASCADE,
+    id_user        INTEGER REFERENCES users (id_user) ON DELETE CASCADE,
     id_poll_option INTEGER REFERENCES poll_option (id_poll_option) ON DELETE CASCADE,
     PRIMARY KEY (
         id_user,
