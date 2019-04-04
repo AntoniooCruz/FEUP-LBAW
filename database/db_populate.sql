5e31d6431722ec86bc7d79e1660ec8aa0a17b1d2
Unknown <bea.smendes98@gmail.com>
2019-04-04 15:37:19 +0100

    started populating db

---
+++ database/db_populate.sql
@@ -0,0 +1,102 @@
+--users
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (1, 'ckinsett0', 'ckinsett0@edublogs.org', 'Corabel Kinsett', 'TSB0eROu', 'libero quis orci nullam molestie nibh in lectus pellentesque at', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (2, 'mbettaney1', 'mbettaney1@dailymotion.com', 'Mollie Bettaney', 'FmXGkoBIGmn9', 'quam fringilla rhoncus mauris enim leo rhoncus sed vestibulum sit', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (3, 'scrumbleholme2', 'scrumbleholme2@bing.com', 'Spence Crumbleholme', 'ZZtIKj5uC', 'cubilia curae nulla dapibus dolor vel est donec odio justo sollicitudin ut', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (4, 'sthursby3', 'sthursby3@seesaa.net', 'Salomo Thursby', 'Y4cTlVlj3Z', 'lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (5, 'ktarver4', 'ktarver4@army.mil', 'Konrad Tarver', 'mW7vK9bs', 'sagittis nam congue risus semper porta volutpat quam pede lobortis ligula sit', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (6, 'emanna5', 'emanna5@sina.com.cn', 'Eveleen Manna', 'vI7kN1BRi16', 'eget rutrum at lorem integer tincidunt ante vel ipsum praesent', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (7, 'ghymor6', 'ghymor6@umich.edu', 'Gideon Hymor', '5ZHM8CPjR', 'placerat ante nulla justo aliquam quis turpis eget elit sodales', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (8, 'epavett7', 'epavett7@quantcast.com', 'Egon Pavett', 'rcg22aA', null, true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (9, 'ksilk8', 'ksilk8@tuttocitta.it', 'Kahlil Silk', 'o0Perg2iAzH', null, true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (10, 'dclouston9', 'dclouston9@istockphoto.com', 'Dru Clouston', 'eEKLhABcSu', 'aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (11, 'corvissa', 'corvissa@macromedia.com', 'Clair Orviss', 'mXmbfC', 'volutpat quam pede lobortis ligula sit amet eleifend pede libero', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (12, 'fbraderb', 'fbraderb@themeforest.net', 'Flossy Brader', 'coelqXfqaZ', null, true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (13, 'pleethemc', 'pleethemc@shinystat.com', 'Pavia Leethem', 'qNQ4hT', null, true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (14, 'hdyetd', 'hdyetd@nymag.com', 'Hephzibah Dyet', '3RgbtTKZpo', 'donec quis orci eget orci vehicula condimentum curabitur in libero ut', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (15, 'cbassete', 'cbassete@bluehost.com', 'Carena Basset', 'FMeFH8sZo9a', 'morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (16, 'kfoulshamf', 'kfoulshamf@wikipedia.org', 'Krissie Foulsham', 'IR6yWweFxNy', null, true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (17, 'dleaningg', 'dleaningg@upenn.edu', 'Den Leaning', 'V4PDCs0', 'urna ut tellus nulla ut erat id mauris vulputate elementum nullam', false, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (18, 'einglebyh', 'einglebyh@newyorker.com', 'Euell Ingleby', 'hidGQI', null, true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (19, 'dspandleyi', 'dspandleyi@macromedia.com', 'Doralin Spandley', '580NEGW3T2Rc', 'posuere metus vitae ipsum aliquam non mauris morbi non lectus', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (20, 'wgethinsj', 'wgethinsj@army.mil', 'Willem Gethins', 'aJ3N8nrJFzI', 'sit amet eleifend pede libero quis orci nullam molestie nibh in', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (21, 'jstoodk', 'jstoodk@umn.edu', 'Joanie Stood', 'wAIXNLMhdS09', 'potenti cras in purus eu magna vulputate luctus cum sociis', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (22, 'kgreatlandl', 'kgreatlandl@google.fr', 'Kara Greatland', 'USr5eIwtxV', 'nam congue risus semper porta volutpat quam pede lobortis ligula sit amet', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (23, 'cwelberrym', 'cwelberrym@pbs.org', 'Chadd Welberry', 'fX7jl07f', null, true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (24, 'cbasilen', 'cbasilen@microsoft.com', 'Celia Basile', 'icEtUlFMz6', 'lectus vestibulum quam sapien varius ut blandit non interdum in', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (25, 'ceateso', 'ceateso@icio.us', 'Corrine Eates', 'ebv2jtW7o5r', 'felis donec semper sapien a libero nam dui proin leo', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (26, 'chargessp', 'chargessp@seesaa.net', 'Celia Hargess', '1JpDr6RVN', 'faucibus cursus urna ut tellus nulla ut erat id mauris vulputate', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (27, 'amoreingq', 'amoreingq@discovery.com', 'Amby Moreing', '0F6kOFu7', 'quisque ut erat curabitur gravida nisi at nibh in hac', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (28, 'acookmanr', 'acookmanr@163.com', 'Abigael Cookman', 'X9Dvrob8Uirq', 'imperdiet sapien urna pretium nisl ut volutpat sapien arcu sed', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (29, 'aloebers', 'aloebers@sogou.com', 'Angil Loeber', 'IkA2g67Gp3e', 'interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et', true, false);
+insert into users (id_user, username, email, name, password, description, active, is_admin) values (30, 'vcreaseyt', 'vcreaseyt@nyu.edu', 'Vern Creasey', 'vUelwm', 'sit amet consectetuer adipiscing elit proin risus praesent lectus vestibulum quam sapien', true, false);
+
+--category
+insert into category (id_category, name) values (1, 'Food');
+insert into category (id_category, name) values (2, 'Film');
+insert into category (id_category, name) values (3, 'Fitness');
+insert into category (id_category, name) values (4, 'Music');
+insert into category (id_category, name) values (5, 'Sports');
+insert into category (id_category, name) values (6, 'Party');
+insert into category (id_category, name) values (7, 'Tech');
+insert into category (id_category, name) values (8, 'Workshop');
+insert into category (id_category, name) values (9, 'Other');
+
+
+--event
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (1, 'Configurable systematic support', '1/11/2018', '12/1/2018', '8446 Rockefeller Parkway', 'ut at dolor quis odio consequat varius', 127, 346, true, 42, 6, 'Vukatanë');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (2, 'Profound attitude-oriented orchestration', '11/30/2017', '6/8/2018', '95 Golf Center', 'lobortis est phasellus sit amet', 149, 483, false, 48, 8, 'Ajman');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (3, 'Implemented responsive approach', '11/3/2017', '10/5/2018', '877 Dayton Junction', 'libero nam dui proin leo odio porttitor id consequat in consequat ut', 126, 208, true, 3, 5, 'Xishan');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (4, 'Operative secondary projection', '2/1/2018', '12/3/2018', '4 Bowman Park', 'sapien iaculis congue vivamus metus arcu adipiscing', 176, 451, false, 45, 2, 'Palaran');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (5, 'Advanced intangible knowledge user', '10/29/2017', '3/25/2019', '81249 Summit Terrace', 'accumsan felis ut', 74, 399, true, 33, 9, 'Banjar Pekandelan');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (6, 'Configurable asymmetric time-frame', '11/21/2017', '1/31/2019', '71785 Mayer Court', 'morbi quis tortor', 81, 467, false, 17, 8, 'Rungis');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (7, 'Re-contextualized clear-thinking paradigm', '10/13/2017', '2/7/2019', '264 Texas Plaza', 'luctus', 110, 376, false, 4, 5, 'Buga');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (8, 'Polarised real-time forecast', '3/5/2018', '9/17/2018', '588 Dayton Circle', 'nascetur ridiculus mus vivamus vestibulum sagittis sapien cum', 28, 96, false, 33, 5, 'Vinež');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (9, 'Universal background core', '9/8/2017', '10/27/2018', '621 Maple Road', 'at nunc commodo placerat praesent blandit nam', 21, 191, false, 35, 9, 'Martapura');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (10, 'Inverse national local area network', '1/21/2018', '1/30/2019', '3261 Pond Drive', 'nulla', 49, 136, true, 33, 9, 'Gaojingzhuang');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (11, 'Profound 3rd generation internet solution', '2/9/2018', '5/6/2018', '012 Forest Pass', 'non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit', 21, 230, false, 16, 8, 'Sarimukti Kaler');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (12, 'Customer-focused non-volatile leverage', '1/30/2018', '3/23/2019', '39189 Morningstar Road', 'justo lacinia eget tincidunt eget tempus vel pede morbi porttitor lorem', 122, 51, true, 2, 4, 'Shanhe');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (13, 'Persistent leading edge application', '10/21/2017', '12/21/2018', '365 Trailsway Junction', 'convallis morbi odio odio elementum eu interdum eu tincidunt in', 169, 481, true, 47, 8, 'Vistino');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (14, 'Centralized eco-centric utilisation', '9/26/2017', '1/24/2019', '0167 Sloan Street', 'non velit nec nisi', 112, 57, true, 19, 1, '‘Amrān');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (15, 'Multi-layered analyzing website', '9/15/2017', '5/15/2018', '2583 Monument Hill', 'ipsum primis in faucibus orci luctus et', 98, 287, true, 36, 4, 'Lincoln');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (16, 'Devolved exuding benchmark', '3/25/2018', '2/3/2019', '675 Morningstar Plaza', 'vestibulum ante ipsum', 124, 379, true, 47, 8, 'Saint-Gaudens');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (17, 'Progressive motivating methodology', '11/1/2017', '11/22/2018', '5807 Bartelt Street', 'in felis eu sapien cursus vestibulum proin eu', 88, 215, true, 30, 2, 'Krasnokamensk');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (18, 'Front-line tertiary policy', '9/8/2017', '5/7/2018', '42474 Ohio Trail', 'non mi integer ac neque duis', 68, 78, true, 21, 4, 'Nicolas Bravo');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (19, 'Phased bi-directional firmware', '12/10/2017', '8/21/2018', '980 Anderson Plaza', 'at turpis a pede', 125, 205, true, 27, 7, 'Firminópolis');
+insert into event (id_event, title, date_created, date, location, description, price, capacity, isPrivate, id_owner, id_category, city) values (20, 'Pre-emptive regional artificial intelligence', '11/16/2017', '7/5/2018', '36141 Westerfield Avenue', 'placerat praesent blandit nam nulla integer pede justo lacinia eget', 107, 183, false, 32, 7, 'Gonghe');
+
+--personal
+
+insert into personal (id_user) values (1);
+insert into personal (id_user) values (2);
+insert into personal (id_user) values (3);
+insert into personal (id_user) values (4);
+insert into personal (id_user) values (5);
+insert into personal (id_user) values (6);
+insert into personal (id_user) values (7);
+insert into personal (id_user) values (8);
+insert into personal (id_user) values (9);
+insert into personal (id_user) values (10);
+insert into personal (id_user) values (11);
+insert into personal (id_user) values (12);
+insert into personal (id_user) values (13);
+insert into personal (id_user) values (14);
+insert into personal (id_user) values (15);
+insert into personal (id_user) values (16);
+insert into personal (id_user) values (17);
+insert into personal (id_user) values (18);
+insert into personal (id_user) values (19);
+insert into personal (id_user) values (20);
+
+
+--business
+insert into business (id_user, verification, website) values (21, 'Approved', 'https://businessinsider.com');
+insert into business (id_user, verification, website) values (22, 'Approved', 'https://europa.eu');
+insert into business (id_user, verification, website) values (23, 'Pending', 'https://studiopress.com');
+insert into business (id_user, verification, website) values (24, 'Approved', 'https://creativecommons.org');
+insert into business (id_user, verification, website) values (25, 'Pending', 'http://bbc.co.uk');
+insert into business (id_user, verification, website) values (26, 'Pending', 'https://harvard.edu');
+insert into business (id_user, verification, website) values (27, 'Approved', 'http://cnn.com');
+insert into business (id_user, verification, website) values (28, 'Approved', 'https://webs.com');
+insert into business (id_user, verification, website) values (29, 'Pending', 'http://chronoengine.com');
+insert into business (id_user, verification, website) values (30, 'Approved', 'http://yahoo.com');
+
