59cde55f6bf8d13f3f56413f46d6db8dc4d43a55
Malheiro <jonas>
2019-06-04 15:04:56 +0100

    comments added on ajax, needs html change on handler

--- app/Comment.php
+++ app/Comment.php
@@ -8,9 +8,17 @@ class Comment extends Model
 {
     protected $table = 'comment';
 
+    protected $primaryKey = 'id_comment';
+
+    public $timestamps  = false;    
+
+    protected $fillable = [
+        'text', 'id_post', 'id_parent_comment', 'id_author', 'date'
+    ];
+
     public function post() {
 
-        return $this->belongstToOne('App\Post', 'id_post', 'id_post');
+        return $this->belongsTo('App\Post', 'id_post', 'id_post');
     }
 
     
