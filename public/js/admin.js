b8a705851e082bd802a17b7cc94b8c44bb7b5d9d
Bea <bea.smendes98@gmail.com>
2019-06-06 23:19:40 +0100

    ban user through report

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,11 +1,20 @@
-let banUserBtn = document.querySelector('.banUser');
+let banUserBtn = document.querySelectorAll('.banUser');
 
 if(banUserBtn!=null){
-banUserBtn.onclick = function(){
-    sendAjaxRequest("put",'/report/'+ document.querySelector('.id_report').innerHTML+'/accept',null,acceptBanUserHandler); 
-}
+banUserBtn.forEach(element => {
+    element.onclick = function(){
+        let id_report = element.getAttribute('repid');
+        sendAjaxRequest("put",'/report/'+ id_report+'/accept',null,acceptBanUserHandler); 
+    }
+});
 }
 
+
 function acceptBanUserHandler(){
-    console.log(this);
+    if(this.status==200){
+        let response = JSON.parse(this.response);
+        let alert = document.createElement('div');
+        alert.innerHTML = '<div id="ticketAlert" class="alert alert-success" role="alert"> User banned!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+        document.getElementById(response['id_report']).replaceWith(alert);
+    }
 }
