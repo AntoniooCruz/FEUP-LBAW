a704fc85e0663f6ae7f4bef1530fcf07c1fe6f2a
Bea <bea.smendes98@gmail.com>
2019-06-07 00:38:34 +0100

    admin blade

--- resources/views/pages/admin.blade.php
+++ resources/views/pages/admin.blade.php
@@ -7,26 +7,64 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <nav>
-      <div class="nav nav-tabs mt-3" id="nav-admin" role="tablist">
-        <a class="nav-item nav-link active" id="nav-users-tab" data-toggle="tab" href="#nav-users" role="tab"
-          aria-controls="nav-users" aria-selected="true"><i class="fas fa-user"></i> Users</a>
-        <a class="nav-item nav-link" id="nav-comments-tab" data-toggle="tab" href="#nav-comments" role="tab"
-          aria-controls="nav-comments" aria-selected="false"><i class="fas fa-comment"></i> Comments</a>
-        <a class="nav-item nav-link" id="nav-events-tab" data-toggle="tab" href="#nav-events" role="tab"
-          aria-controls="nav-events" aria-selected="false"><i class="fas fa-calendar"></i> Events</a>
+<div id="wrapper">
+<ul class="nav nav-tabs sidebar navbar-nav" id="nav-admin2" role="tablist">
+    <li class="nav-item" role="tablist">
+      <a class="nav-item nav-link active" id="nav-users-tab" data-toggle="tab" href="#activeUsers" role="tab"
+      aria-controls="nav-users" aria-selected="true">
+      <i class="fas fa-user"></i>
+        <span>Users</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-item nav-link" id="nav-comments-tab" data-toggle="tab" href="#nav-comments" role="tab"
+      aria-controls="nav-comments" aria-selected="false">
+      <i class="fas fa-comment"></i>
+        <span>Comments</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-item nav-link" id="nav-verify-tab" data-toggle="tab" href="#nav-verify" role="tab"
+      aria-controls="nav-verify" aria-selected="false">
+      <i class="fas fa-calendar"></i>
+        <span>Events</span></a>
+    </li>
+    <li class="nav-item">
         <a class="nav-item nav-link" id="nav-verify-tab" data-toggle="tab" href="#nav-verify" role="tab"
-          aria-controls="nav-verify" aria-selected="false"><i class="fas fa-check-circle"></i> Verify</a>
-      </div>
-    </nav>
+        aria-controls="nav-verify" aria-selected="false">
+        <i class="fas fa-check-circle"></i>
+                  <span>Verify</span></a>
+      </li>
+  </ul>
+<div class="container">
     <div class="tab-content" id="nav-admintab">
       <div class="tab-pane fade show active" id="nav-users" role="tabpanel" aria-labelledby="nav-users-tab">
+          <nav class="mb-2">
+              <div class="nav nav-tabs mt-3" id="nav-admin" role="tablist">
+                <a class="nav-item nav-link active" id="nav-users-tab" data-toggle="tab" href="#activeUsers" role="tab"
+                  aria-controls="nav-users" aria-selected="true"><i class="fas fa-inbox"></i> Pending</a>
+                <a class="nav-item nav-link" id="nav-comments-tab" data-toggle="tab" href="#archivedUsers" role="tab"
+                  aria-controls="nav-comments" aria-selected="false"><i class="fas fa-archive"></i> Archived</a>
+              </div>
+            </nav>
+        <div id="activeUsers" class="tab-pane fade show active">
         @foreach ($userReports as $rep)
           @include ('partials.user-report',['report'=>$rep])
         @endforeach
+      </div> 
+      <div id="archivedUsers" class="tab-pane fade">
+          @foreach ($userReports as $rep)
+          @endforeach
+        </div> 
       </div>  
       <div class="tab-pane fade" id="nav-comments" role="tabpanel" aria-labelledby="nav-comments-tab">
+          <nav class="mb-2">
+              <div class="nav nav-tabs mt-3" id="nav-admin" role="tablist">
+                <a class="nav-item nav-link active" id="nav-users-tab" data-toggle="tab" href="#" role="tab"
+                  aria-controls="nav-users" aria-selected="true"><i class="fas fa-inbox"></i> Pending</a>
+                <a class="nav-item nav-link" id="nav-comments-tab" data-toggle="tab" href="#" role="tab"
+                  aria-controls="nav-comments" aria-selected="false"><i class="fas fa-archive"></i> Archived</a>
+              </div>
+            </nav>
         <div class="container-fluid actionCard">
           <div class="report report-comment">
             <div class="card card-comment">
@@ -358,5 +396,6 @@
       </div>
     </div>
   </div>
+</div>
 
   @endsection
