b87c0737945ee92ec22d995ab8f0234d48dd34be
Malheiro <jonas>
2019-06-06 19:23:51 +0100

    polls almost working

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -19,6 +19,7 @@ use App\Comment;
 use Carbon\Carbon;
 use App\Invite;
 use App\PollOption;
+use App\Poll;
 use App\VoteOnPoll;
 
 class EventController extends Controller
@@ -239,7 +240,11 @@ class EventController extends Controller
     public function vote($id_poll_option) {
 
         $poll_option = PollOption::find($id_poll_option);
-        
+
+        $poll = Poll::find($poll_option->id_poll);
+
+        $post = Post::find($poll->id_post);
+
         if (!Auth::check()) 
             return response(403);
 
@@ -268,9 +273,11 @@ class EventController extends Controller
         if($oldPollOptId!=null){
             $temp1 = VoteOnPoll::where('id_poll_option', $oldPollOptId)->count();
             $oldPerc = floor(($temp1/$noVotesTotal)*100);
-        }else $oldPerc = null;
+        } else $oldPerc = null;
 
-        return response()->json(['perc'=>$perc, 'oldPollOptId'=>$oldPollOptId, 'oldPerc'=>$oldPerc], 200);
+        $pollOptionsIds = $post->poll->pollOptions()->get();
+
+        return response()->json(['perc'=>$perc, 'oldPollOptId' =>$oldPollOptId, 'noVotesTotal' => $noVotesTotal, 'pollOptsID' => $pollOptionsIds, 200]);
 
         
     }
--- public/js/poll.js
+++ public/js/poll.js
@@ -1,32 +1,55 @@
 let votePollBttn = document.querySelectorAll('#poll-option1');
 if (votePollBttn != null) {
-  for(var j= 0; j < votePollBttn.length; j++){
+  for (var j = 0; j < votePollBttn.length; j++) {
     votePollBttn[j].addEventListener('click', addVoteOnPollRequest);
-   }
+  }
 }
 
 let poll_option_id;
 
 function addVoteOnPollRequest(evt) {
-    
-    poll_option_id = evt.path[0].dataset.id;
-  
-    let method = 'post';
-  
-    sendAjaxRequest(method, '/api/pollOption/' + poll_option_id, null, addVoteOnPollRequestHandler);
+
+  poll_option_id = evt.path[0].dataset.id;
+
+  let method = 'post';
+
+  sendAjaxRequest(method, '/api/pollOption/' + poll_option_id, null, addVoteOnPollRequestHandler);
+}
+
+function addVoteOnPollRequestHandler() {
+  console.log(this);
+
+  if (this.status == 200) {
+    let perc = JSON.parse(this.response)['perc'] + "%";
+    let progress = document.getElementById(poll_option_id);
+    progress.style.width = perc;
+    progress.nextSibling.innerHTML = perc;
+
+    let oldId = JSON.parse(this.response)['oldPollOptId'];
+    let noVotes = JSON.parse(this.response)['noVotesTotal'];
+
+    let arr = JSON.parse(this.response)['pollOptsID']
+
+    update(arr,poll_option_id, oldId, noVotes);
   }
+}
 
-  function addVoteOnPollRequestHandler() {
-    console.log(this);
+  function update(arr,incID, decID, noVotes) {
+    
+    for(var i=0; i < arr.length; i++){
+      let pollOpt = document.getElementById(arr[i].id_poll_option);
+      if(arr[i].id_poll_option == incID){
+        pollOpt.setAttribute("data-name", parseInt(pollOpt.dataset.name)+ 1);
 
-    if (this.status == 200) {
-        let perc = JSON.parse(this.response)['perc'] + "%";
-        let progress = document.getElementById(poll_option_id);
-        progress.style.width = perc;
-        progress.nextSibling.innerHTML = perc;
-
-        let oldperc = JSON.parse(this.response)['oldperc'];
-
+      } else if(decID != null) {
+          if(arr[i].id_poll_option == decID) {
+            pollOpt.setAttribute("data-name", parseInt(pollOpt.dataset.name) - 1);
+          }
       }
-
+      let percent = Math.floor(pollOpt.dataset.name/noVotes*100);
+      console.log(noVotes);
+      console.log(pollOpt.dataset.name);
+      pollOpt.style.width = percent+ "%";
+      pollOpt.nextSibling.innerText = percent.toString().concat('%')
+    }
   }
