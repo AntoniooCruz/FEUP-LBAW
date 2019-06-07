2ebb36dbf52272a29a8c34fc06ec5eaab9eaaade
Unknown <up201603811@fe.up.pt>
2019-06-07 04:21:11 +0100

    About no lorem ipsum

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -16,15 +16,15 @@
     <div id="titles" class="row">
         <div class="col-md-6  px-4">
             <h4>ABOUT US</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et
-                dolore magna aliqua</p>
+            <p> RainCheck’s purpose is to help both businesses and individuals create, manage and share their events.</P>
+            <p>This will allow companies to increase their client reach through an event management web application.</P>
+            
         </div>
         <div id="contacts" class="col-md-6  px-4">
             <h4>CONTACT US</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et
-                dolore magna aliqua</p>
+            <p>+351 917 633 662</p>
+            <p>+351 22 508 1400 </p>
+            <p>R. Dr. Roberto Frias, 4200-465 Porto, Portugal</p>
         </div>
     </div>
 </section>
